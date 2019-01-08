ec4de2e71d393d66046f966854c4deb4c051f9ba
Waldemar Modzelewski <waldemar.modzelewski@cgi.com>
2019-01-08 15:41:18 +0100

    az vm create

--- createResourceGroup.sh
+++ createResourceGroup.sh
@@ -13,12 +13,16 @@ else
 fi
 
 echo "Processing group "$resourceGroupName" in location "$location
-rgExists="A"$(az group exists --name $resourceGroupName)
+rgExists=$(az group exists --name $resourceGroupName)
 echo "$rgExists"
 
-if [ $rgExists = "Afalse" ]; then
+if [ "$rgExists" = "false" ]; then
 	echo "Creating group "$resourceGroupName;
 	az group create --name $resourceGroupName --location $location;
 else
 	echo "Group "$resourceGroupName " exists";
 fi
+
+
+#az vm create --resource-group RG_AIL_CH1 --name ailVmUbuntu1 --image UbuntuLTS --size Standard_B1s --ssh-key-value @~/.ssh/id_rsa.pub
+#az vm open-port -g RG_AIL_CH1 --name ailVmUbuntu1 --port 80
